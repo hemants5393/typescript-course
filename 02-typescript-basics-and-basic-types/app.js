b082e055db8fe34b19b08a0e1a1a75edf98b878e
DESKTOP-NICHS2L\heman <hemant.s5393@gmail.com>
2021-09-16 22:37:05 +0530

    Object Types

--- 02-typescript-basics-and-basic-types/app.js
+++ 02-typescript-basics-and-basic-types/app.js
@@ -29,3 +29,34 @@
     var resultPhrase = "Result is: ";
     add(number1, number2, printResult, resultPhrase);
 })();
+/*
+    Object Types
+*/
+(function () {
+    // Object with inferred type
+    var person = {
+        name: "Hemant Singh",
+        age: 29
+    };
+    console.log("Person object:", person);
+    console.log("Person name:", person.name);
+    // Object with explicit type
+    var player = {
+        name: "Virat Kohli",
+        type: "Batter",
+        age: 33
+    };
+    console.log("Player object:", player);
+    console.log("Player name:", player.name);
+    // Nested object
+    var product = {
+        id: "abc1",
+        price: 12.99,
+        tags: ["great-offer", "hot-and-new"],
+        details: {
+            title: "Red Carpet",
+            description: "A great carpet - almost brand-new!"
+        }
+    };
+    console.log("Product nested object:", product);
+})();
--- 02-typescript-basics-and-basic-types/app.ts
+++ 02-typescript-basics-and-basic-types/app.ts
@@ -37,3 +37,41 @@
   const resultPhrase = "Result is: ";
   add(number1, number2, printResult, resultPhrase);
 })();
+
+/*
+    Object Types
+*/
+(function () {
+  // Object with inferred type
+  const person = {
+    name: "Hemant Singh",
+    age: 29,
+  };
+  console.log("Person object:", person);
+  console.log("Person name:", person.name);
+
+  // Object with explicit type
+  const player: {
+    name: string;
+    type: string;
+    age: number;
+  } = {
+    name: "Virat Kohli",
+    type: "Batter",
+    age: 33,
+  };
+  console.log("Player object:", player);
+  console.log("Player name:", player.name);
+
+  // Nested object
+  const product = {
+    id: "abc1",
+    price: 12.99,
+    tags: ["great-offer", "hot-and-new"],
+    details: {
+      title: "Red Carpet",
+      description: "A great carpet - almost brand-new!",
+    },
+  };
+  console.log("Product nested object:", product);
+})();
