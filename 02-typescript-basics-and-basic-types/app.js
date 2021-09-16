fcd1d288129802b5ff208b54e00db97a7ef45c63
DESKTOP-NICHS2L\heman <hemant.s5393@gmail.com>
2021-09-16 21:07:22 +0530

    02-Working with numbers, string and booleans

--- 02-typescript-basics-and-basic-types/app.js
+++ 02-typescript-basics-and-basic-types/app.js
@@ -10,3 +10,22 @@
     var result = add(number1, number2);
     console.log("add() Result:", result);
 })();
+/*
+    Working with numbers, string and booleans
+*/
+(function () {
+    function add(n1, n2, showResult, resultPhrase) {
+        var result = n1 + n2;
+        if (showResult) {
+            console.log(resultPhrase, result);
+        }
+        else {
+            return result;
+        }
+    }
+    var number1 = 15;
+    var number2 = 5.9;
+    var printResult = true;
+    var resultPhrase = "Result is: ";
+    add(number1, number2, printResult, resultPhrase);
+})();
--- 02-typescript-basics-and-basic-types/app.ts
+++ 02-typescript-basics-and-basic-types/app.ts
@@ -12,3 +12,28 @@
   const result = add(number1, number2);
   console.log("add() Result:", result);
 })();
+
+/*
+    Working with numbers, string and booleans
+*/
+(function () {
+  function add(
+    n1: number,
+    n2: number,
+    showResult: boolean,
+    resultPhrase: string
+  ) {
+    const result = n1 + n2;
+    if (showResult) {
+      console.log(resultPhrase, result);
+    } else {
+      return result;
+    }
+  }
+
+  const number1 = 15;
+  const number2 = 5.9;
+  const printResult = true;
+  const resultPhrase = "Result is: ";
+  add(number1, number2, printResult, resultPhrase);
+})();
