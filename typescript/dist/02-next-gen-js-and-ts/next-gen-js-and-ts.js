5bbf7ffd91548368bd522c4d999fb504ae18eddf
DESKTOP-NICHS2L\heman <hemant.s5393@gmail.com>
2021-09-18 16:03:22 +0530

    02-next-gen-js-and-ts Initial commit

---
+++ typescript/dist/02-next-gen-js-and-ts/next-gen-js-and-ts.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("Message from next-gen-js-and-ts file.");
+//# sourceMappingURL=next-gen-js-and-ts.js.map
---
+++ typescript/src/02-next-gen-js-and-ts/next-gen-js-and-ts.ts
@@ -0,0 +1,1 @@
+console.log("Message from next-gen-js-and-ts file.")
