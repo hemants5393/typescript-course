b06547e1a11a1e9eed9fecd6c210f2464fb8c061
DESKTOP-NICHS2L\heman <hemant.s5393@gmail.com>
2021-09-18 12:06:05 +0530

    03-Setting compilation target to "es6"

--- 03-typescript-configuration/src/app.js
+++ 03-typescript-configuration/src/app.js
@@ -6,9 +6,9 @@
     function add(n1, n2) {
         return n1 + n2;
     }
-    var number1 = 5;
-    var number2 = 15;
-    var result = add(number1, number2);
+    const number1 = 5;
+    const number2 = 15;
+    const result = add(number1, number2);
     console.log("add() Result:", result);
 })();
 /*
@@ -16,7 +16,7 @@
 */
 (function () {
     function add(n1, n2, showResult, resultPhrase) {
-        var result = n1 + n2;
+        const result = n1 + n2;
         if (showResult) {
             console.log(resultPhrase, result);
         }
@@ -24,10 +24,10 @@
             return result;
         }
     }
-    var number1 = 15;
-    var number2 = 5.9;
-    var printResult = true;
-    var resultPhrase = "Result is: ";
+    const number1 = 15;
+    const number2 = 5.9;
+    const printResult = true;
+    const resultPhrase = "Result is: ";
     add(number1, number2, printResult, resultPhrase);
 })();
 /*
@@ -35,14 +35,14 @@
 */
 (function () {
     // Object with inferred type
-    var person = {
+    const person = {
         name: "Hemant Singh",
         age: 29,
     };
     console.log("Person object:", person);
     console.log("Person name:", person.name);
     // Object with explicit type
-    var player = {
+    const player = {
         name: "Virat Kohli",
         type: "Batter",
         age: 33,
@@ -50,7 +50,7 @@
     console.log("Player object:", player);
     console.log("Player name:", player.name);
     // Nested object
-    var product = {
+    const product = {
         id: "abc1",
         price: 12.99,
         tags: ["great-offer", "hot-and-new"],
@@ -65,10 +65,10 @@
     Array Types
 */
 (function () {
-    var sports;
+    let sports;
     sports = ["Cricket", "Football", "Hockey"];
     console.log("Sports array:", sports);
-    var person = {
+    const person = {
         name: "Hemant Singh",
         age: 29,
         hobbies: ["Sports", "Video Games"],
@@ -79,9 +79,9 @@
     Tuple Types
 */
 (function () {
-    var person = ["Hemant Singh", 29];
+    let person = ["Hemant Singh", 29];
     console.log("Person tuple:", person);
-    var employee = {
+    let employee = {
         name: "Hemant Singh",
         age: 29,
         designation: [2, "Developer"],
@@ -92,13 +92,13 @@
     Enum Types
 */
 (function () {
-    var Designation;
+    let Designation;
     (function (Designation) {
         Designation[Designation["TESTER"] = 0] = "TESTER";
         Designation[Designation["DEVOPS"] = 1] = "DEVOPS";
         Designation[Designation["DEVELOPER"] = 2] = "DEVELOPER";
     })(Designation || (Designation = {}));
-    var employee = {
+    let employee = {
         name: "Hemant Singh",
         age: 29,
         designation: Designation.DEVELOPER,
@@ -108,7 +108,7 @@
         console.log(employee.name, " is a Developer.");
     }
     // Initialized enum
-    var APIS;
+    let APIS;
     (function (APIS) {
         APIS["GET_POSTS"] = "version3/getPosts";
         APIS["POST_POST"] = "version3/postPost";
@@ -116,7 +116,7 @@
     })(APIS || (APIS = {}));
     console.log("Get posts api:", APIS.GET_POSTS);
     // Enum with both number and string values
-    var CRICKET;
+    let CRICKET;
     (function (CRICKET) {
         CRICKET[CRICKET["NO_PLAYERS"] = 11] = "NO_PLAYERS";
         CRICKET["TEAM"] = "India";
@@ -130,7 +130,7 @@
     "any" Type
 */
 (function () {
-    var data;
+    let data;
     data = ["Cricket", 25];
     console.log("data of any type:", data);
 })();
@@ -138,7 +138,7 @@
     Union Types
 */
 (function () {
-    var data;
+    let data;
     data = "A string value.";
     console.log("data as union type of string and number:", data);
     data = 30;
@@ -149,17 +149,17 @@
 */
 (function () {
     // Simple string literal type
-    var action;
+    let action;
     action = "come";
     console.log("simple string literal type:", action);
-    var easing;
+    let easing;
     easing = "ease-out";
     console.log("string literal type with custom type:", easing);
     // Simple number literal type
-    var pointsScored;
+    let pointsScored;
     pointsScored = 5;
     console.log("simple number literal type:", action);
-    var player = {
+    let player = {
         name: "Harry Potter",
         health: 95,
         levels: 2,
@@ -171,9 +171,9 @@
     Type Aliases/ Custom types
 */
 (function () {
-    var data = "A string value.";
+    let data = "A string value.";
     console.log("simple type alias example:", data);
-    var user = {
+    let user = {
         name: "Hemant Singh",
         age: 29,
     };
@@ -187,15 +187,15 @@
     function add(n1, n2) {
         return n1 + n2;
     }
-    var number1 = 5;
-    var number2 = 15;
-    var result = add(number1, number2);
+    const number1 = 5;
+    const number2 = 15;
+    const result = add(number1, number2);
     console.log("add() Result:", result);
     // Function with void return type
     function printMessage(message) {
         console.log("Message is: ", message);
     }
-    var message = "Hello, how are you?";
+    let message = "Hello, how are you?";
     printMessage(message);
 })();
 /*
@@ -206,14 +206,14 @@
     function greaterThan(n1, n2) {
         return n1 > n2;
     }
-    var number1 = 5;
-    var number2 = 15;
-    var greaterThanFun;
+    const number1 = 5;
+    const number2 = 15;
+    let greaterThanFun;
     greaterThanFun = greaterThan; // storing the function with the same signature
     console.log("greaterThanFun result:", greaterThanFun(number1, number2));
     // Function type with callback
     function addAndHandle(num1, num2, cb) {
-        var result = num1 + num2;
+        const result = num1 + num2;
         cb(result);
     }
     function printResult(result) {
