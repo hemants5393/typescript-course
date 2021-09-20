1b11080a4616472a0ac0073477d1dfe2b0c26473
DESKTOP-NICHS2L\heman <hemant.s5393@gmail.com>
2021-09-20 17:36:25 +0530

    03-Access modifiers: private, public and protected

--- typescript/dist/03-classes-and-interfaces/classes-and-interfaces.js
+++ typescript/dist/03-classes-and-interfaces/classes-and-interfaces.js
@@ -21,4 +21,55 @@ console.log("******************03 classes and interfaces******************");
     const accounting = new Department("Accounting");
     accounting.describe();
 })();
+(function () {
+    (function () {
+        class Employee {
+            constructor(code, name) {
+                this.empCode = code;
+                this.empName = name;
+            }
+            printName() {
+                console.log("Employee name:", this.empName);
+            }
+        }
+        let employee = new Employee(123, "Hemant Singh");
+        employee.printName();
+    })();
+    (function () {
+        class Employee {
+            constructor(code, name) {
+                this.empCode = code;
+                this.empName = name;
+            }
+            printName() {
+                console.log("Employee name:", this.empName);
+            }
+            doSomething() {
+                const code = this.empCode;
+                console.log("Employee code:", code);
+            }
+        }
+        let employee = new Employee(123, "Hemant Singh");
+    })();
+    (function () {
+        class Employee {
+            constructor(name, code) {
+                this.company = "ABC";
+                this.empName = name;
+                this.empCode = code;
+            }
+        }
+        class SalesEmployee extends Employee {
+            constructor(name, code, department) {
+                super(name, code);
+                this.department = department;
+            }
+            doSomething() {
+                const code = this.empCode;
+            }
+        }
+        let empObj = new SalesEmployee("John Smith", 123, "Sales");
+        console.log("empObj:", empObj);
+    })();
+})();
 //# sourceMappingURL=classes-and-interfaces.js.map
--- typescript/src/03-classes-and-interfaces/classes-and-interfaces.ts
+++ typescript/src/03-classes-and-interfaces/classes-and-interfaces.ts
@@ -32,3 +32,75 @@ console.log("******************03 classes and interfaces******************");
   const accounting = new Department("Accounting");
   accounting.describe();
 })();
+
+/*
+    Access modifiers
+*/
+(function () {
+  // Example-1 (public)
+  (function () {
+    class Employee {
+      public empCode: number;
+      empName: string;
+      constructor(code: number, name: string) {
+        this.empCode = code;
+        this.empName = name;
+      }
+      public printName(): void {
+        console.log("Employee name:", this.empName);
+      }
+    }
+    let employee = new Employee(123, "Hemant Singh");
+    employee.printName();
+  })();
+
+  // Example-2 (private)
+  (function () {
+    class Employee {
+      private empCode: number;
+      empName: string;
+      constructor(code: number, name: string) {
+        this.empCode = code;
+        this.empName = name;
+      }
+      public printName(): void {
+        console.log("Employee name:", this.empName);
+      }
+      private doSomething() {
+        const code = this.empCode;
+        console.log("Employee code:", code);
+      }
+    }
+    let employee = new Employee(123, "Hemant Singh");
+    // employee.doSomething(); // It will give an error as doSomething() is private.
+  })();
+
+  // Example-3 (Protected)
+  (function () {
+    class Employee {
+      private company = "ABC";
+      public empName: string;
+      protected empCode: number;
+      constructor(name: string, code: number) {
+        this.empName = name;
+        this.empCode = code;
+      }
+    }
+
+    class SalesEmployee extends Employee {
+      private department: string;
+      constructor(name: string, code: number, department: string) {
+        super(name, code);
+        this.department = department;
+      }
+      public doSomething(): void {
+        // const company = this.company; // Error (private cannot be accessed)
+        const code = this.empCode; // protected can be accessed in extended class
+      }
+    }
+
+    let empObj = new SalesEmployee("John Smith", 123, "Sales");
+    console.log("empObj:", empObj);
+    // empObj.empCode; // Error (protected cannot be accessed outside class)
+  })();
+})();
