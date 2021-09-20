3bfc80ca86cfb1018d1a0e51c1c1749c4cdbc904
DESKTOP-NICHS2L\heman <hemant.s5393@gmail.com>
2021-09-20 23:46:28 +0530

    03-Abstract classes

--- typescript/dist/03-classes-and-interfaces/classes-and-interfaces.js
+++ typescript/dist/03-classes-and-interfaces/classes-and-interfaces.js
@@ -191,4 +191,41 @@ console.log("******************03 classes and interfaces******************");
     let john = new Employee("John", "Doe", "Front-end Developer");
     john.printEmployeeInfo();
 })();
+(function () {
+    class Employee {
+        constructor(firstName, lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        get fullName() {
+            return `${this.firstName} ${this.lastName}`;
+        }
+        compensationStatement() {
+            return `${this.fullName} makes ${this.getSalary()} a month.`;
+        }
+    }
+    class FullTimeEmployee extends Employee {
+        constructor(firstName, lastName, salary) {
+            super(firstName, lastName);
+            this.salary = salary;
+        }
+        getSalary() {
+            return this.salary;
+        }
+    }
+    class Contractor extends Employee {
+        constructor(firstName, lastName, rate, hours) {
+            super(firstName, lastName);
+            this.rate = rate;
+            this.hours = hours;
+        }
+        getSalary() {
+            return this.rate * this.hours;
+        }
+    }
+    let john = new FullTimeEmployee("John", "Doe", 12000);
+    let jane = new Contractor("Jane", "Doe", 100, 160);
+    console.log(john.compensationStatement());
+    console.log(jane.compensationStatement());
+})();
 //# sourceMappingURL=classes-and-interfaces.js.map
--- typescript/src/03-classes-and-interfaces/classes-and-interfaces.ts
+++ typescript/src/03-classes-and-interfaces/classes-and-interfaces.ts
@@ -272,3 +272,52 @@ console.log("******************03 classes and interfaces******************");
   let john = new Employee("John", "Doe", "Front-end Developer");
   john.printEmployeeInfo();
 })();
+
+/*
+    Abstract classes
+*/
+(function () {
+  // Abstract class (Base class)
+  abstract class Employee {
+    constructor(private firstName: string, private lastName: string) {}
+    abstract getSalary(): number;
+    get fullName(): string {
+      return `${this.firstName} ${this.lastName}`;
+    }
+    compensationStatement(): string {
+      return `${this.fullName} makes ${this.getSalary()} a month.`;
+    }
+  }
+
+  // Child class
+  class FullTimeEmployee extends Employee {
+    constructor(firstName: string, lastName: string, private salary: number) {
+      super(firstName, lastName);
+    }
+    getSalary(): number {
+      return this.salary;
+    }
+  }
+
+  // Child class
+  class Contractor extends Employee {
+    constructor(
+      firstName: string,
+      lastName: string,
+      private rate: number,
+      private hours: number
+    ) {
+      super(firstName, lastName);
+    }
+    getSalary(): number {
+      return this.rate * this.hours;
+    }
+  }
+
+  // Instances of child classes
+  let john = new FullTimeEmployee("John", "Doe", 12000);
+  let jane = new Contractor("Jane", "Doe", 100, 160);
+
+  console.log(john.compensationStatement());
+  console.log(jane.compensationStatement());
+})();
